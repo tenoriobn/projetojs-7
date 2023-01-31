887232af1de84b89d1f751dd936539564748d314
Bruno Tenorio <tenoriobn@gmail.com>
2023-01-31 18:08:04 -0300

    Adicionando responsividade para Desktop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,13 @@
 const navOpen = document.getElementById("nav__open");
 const navClose = document.getElementById("nav__close");
+const nav = document.getElementById("nav")
 
 navOpen.addEventListener("click", () => {
-    document.getElementById("nav").style.width = ("100%");
+    nav.style.width = ("100%");
+    nav.style.transition = ("all .5s");
+
 })
 
 navClose.addEventListener("click", () => {
-    document.getElementById("nav").style.width = ("0%");
+    nav.style.width = ("0%");
 })
